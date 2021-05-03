f022db181169eb277acb2bef0e1d1a92c207bcb3
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2021-05-03 19:57:19 +0200

    Create dht22_class.py

---
+++ room/dht22_class.py
@@ -0,0 +1,16 @@
+from conf.Generic_Sensor import *
+import time
+import Adafruit_DHT
+
+class dht22(SensorPublisher):
+    def __init__(self,configuration_filename,device_ID='dht22',pin=17):
+        SensorPublisher.__init__(self,configuration_filename,device_ID)
+        self.DHT22 = Adafruit_DHT.DHT22                              
+        self.DHT22_PIN = pin
+                    
+    def retrieveData(self):
+        humidity, temperature = Adafruit_DHT.read_retry(self.DHT22, self.DHT22_PIN, retries=2, delay_seconds=3)
+        #outputResult=[{'parameter':'humidity','value':humidity},{'parameter':'temperature','value':temperature}]
+        outputResult=[{'parameter':'humidity','value':humidity}]
+        return outputResult
+            
